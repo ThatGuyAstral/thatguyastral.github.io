2e742038259273092e231d4f4a786d8d9e1ddeeb
cosmic <144744086+ThatGuyAstral@users.noreply.github.com>
2024-07-07 17:09:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,20 +5,25 @@ function back()
 
 function projects()
 {
-  window.location.href = 'https://thatguyastral.github.io/projs.html';
+  window.location.href = 'https://thatguyastral.github.io/projs';
 }
     
 function about()
 {
-  window.location.href = 'https://thatguyastral.github.io/about.html';
+  window.location.href = 'https://thatguyastral.github.io/about';
 }
 
 function socials()
 {
-  window.location.href = 'https://thatguyastral.github.io/socials.html';
+  window.location.href = 'https://thatguyastral.github.io/socials';
 }
 
 function discord()
 {
   window.location.href = 'https://discord.gg/EyyfjjXs3n';
+}
+
+function black()
+{
+  window.location.href = 'https://thatguyastral.github.io/black';
 }
